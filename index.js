2f1581dcfccccc33787b80f84a68c42e57e18647
José Gabriel <jgnx@hotmail.com>
2019-11-20 07:07:00 -0300

    #3 show ejs variables in the html page

--- index.js
+++ index.js
@@ -3,8 +3,15 @@ const app = express();
 
 app.set('view engine', 'ejs');
 
-app.get("/", (req, res) => {
-    res.render("index");
+app.get("/:name/:lang", (req, res) => {
+    let name = req.params.name;
+    let lang = req.params.lang;
+    res.render("index", {
+        name: name,
+        lang: lang,
+        companyName: "Chicos",
+        totalOfClients: 5200
+    });
 });
 
 app.listen(8080,()=>{console.log("Server is ok.");});
