b693e8f7be387249cc1cd9d74f45e4784aca62c9
José Gabriel <jgnx@hotmail.com>
2019-11-21 07:16:54 -0300

    Loop with ejs

--- index.js
+++ index.js
@@ -7,7 +7,15 @@ app.get("/:name/:lang", (req, res) => {
     let name = req.params.name;
     let lang = req.params.lang;
     let showMessageError = false;
+
+    let products = [
+        {name: "laranja", price: 3.5},
+        {name: "coca-cola", price: 5.5},
+        {name: "leite", price: 1.57}
+    ];
+
     res.render("index", {
+        products: products,
         name: name,
         lang: lang,
         companyName: "Chicos",
