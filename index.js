4a3b37c663a0c2f629e3ad87a9bf7a15612e15ab
José Gabriel <jgnx@hotmail.com>
2019-11-21 07:07:10 -0300

    Conditional Structure with efs

--- index.js
+++ index.js
@@ -6,11 +6,13 @@ app.set('view engine', 'ejs');
 app.get("/:name/:lang", (req, res) => {
     let name = req.params.name;
     let lang = req.params.lang;
+    let showMessageError = false;
     res.render("index", {
         name: name,
         lang: lang,
         companyName: "Chicos",
-        totalOfClients: 5200
+        totalOfClients: 5200,
+        showError: showMessageError
     });
 });
 
