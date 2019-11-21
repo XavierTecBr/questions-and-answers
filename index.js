6a4987de7d96389bb47892eb1286451f4a4c832d
José Gabriel <jgnx@hotmail.com>
2019-11-21 07:28:33 -0300

    Static file with ejs, photos and css

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const app = express();
 
 app.set('view engine', 'ejs');
+app.use(express.static('public'));
 
 app.get("/:name/:lang", (req, res) => {
     let name = req.params.name;
